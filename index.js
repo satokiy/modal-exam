6339e418af08f54c437385b35d86b4d2e0cb3a58
satokiy <syamada@netprotections.co.jp>
2023-01-03 18:52:10 +0900

    こうかなぁ

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const src =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp";
 const iframeAttr = {
   id: "my-iframe",
-  // src: 'modal.html',
-  src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp",
+  src: 'modal.html',
+  // src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp",
   class: "frame_center",
   frameborder: "0",
   scrolling: "no",
@@ -27,11 +27,25 @@ openButton.innerText = "Open Modal";
 setAttr(openButton, openButtonAttr);
 app.appendChild(openButton);
 
+// close button
+const closeButton = document.createElement('div')
+closeButton.className = 'button-close';
+closeButton.innerHTML = `
+<button class="modal-close">X</button>
+`
+
 // open modal
 openButton.addEventListener("click", function () {
   div.classList.add("overlay");
+  div.appendChild(closeButton);
   div.appendChild(iframe);
 });
+
+closeButton.addEventListener('click', () => {
+ div.removeChild(iframe);
+ div.removeChild(closeButton);
+ div.classList.remove("overlay");  
+})
 
 // close event
 window.addEventListener("message", (e) => {
--- modal.js
+++ modal.js
@@ -1,18 +1,18 @@
-const closeButton = document.getElementsByClassName("modal-close")[0];
+// const closeButton = document.getElementsByClassName("modal-close")[0];
 const otherButton = document.getElementsByClassName('go-other')[0];
 
-closeButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  // 親windowがいるときのみ発火
-  if (window.parent !== window) {
-    const message = JSON.stringify({
-      message: "CANCEL_MODAL",
-    });
-    window.parent.postMessage(message, "*");
-  } else {
-    console.log("no parent. closing.");
-  }
-})
+// closeButton.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   // 親windowがいるときのみ発火
+//   if (window.parent !== window) {
+//     const message = JSON.stringify({
+//       message: "CANCEL_MODAL",
+//     });
+//     window.parent.postMessage(message, "*");
+//   } else {
+//     console.log("no parent. closing.");
+//   }
+// })
 
 otherButton.addEventListener("click", (e) => {
   e.preventDefault();
