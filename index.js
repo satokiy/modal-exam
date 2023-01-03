509d6abd75419ab66b7b72ca36f139fa0fddbfcd
satokiy <syamada@netprotections.co.jp>
2023-01-03 18:31:08 +0900

    こういう実装もありか？

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const div = document.createElement("div");
 app.appendChild(div);
 
 const iframe = document.createElement("iframe");
+const src =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp";
 const iframeAttr = {
   id: "my-iframe",
-  src: "modal.html",
+  // src: 'modal.html',
+  src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp",
   class: "frame_center",
-  height: "800px",
-  width: "600px",
   frameborder: "0",
   scrolling: "no",
 };
@@ -34,12 +35,17 @@ openButton.addEventListener("click", function () {
 
 // close event
 window.addEventListener("message", (e) => {
-  const message = JSON.parse(e.data).message;
-  console.log(message);
+  const data = JSON.parse(e.data);
+  const message = data.message;
 
   if (message === "CANCEL_MODAL") {
-    div.removeChild(iframe);
-    div.classList.remove("overlay");
+   div.removeChild(iframe);
+   div.classList.remove("overlay"); 
+  }
+  if (message === "GO_OTHER") {
+   div.removeChild(iframe);
+   div.classList.remove("overlay");
+   window.location.href = data.url;
   }
 });
 
--- modal.js
+++ modal.js
@@ -1,4 +1,5 @@
 const closeButton = document.getElementsByClassName("modal-close")[0];
+const otherButton = document.getElementsByClassName('go-other')[0];
 
 closeButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -6,6 +7,20 @@ closeButton.addEventListener("click", (e) => {
   if (window.parent !== window) {
     const message = JSON.stringify({
       message: "CANCEL_MODAL",
+    });
+    window.parent.postMessage(message, "*");
+  } else {
+    console.log("no parent. closing.");
+  }
+})
+
+otherButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  // 親windowがいるときのみ発火
+  if (window.parent !== window) {
+    const message = JSON.stringify({
+      message: "GO_OTHER",
+      url: "https://www.netprotections.com/",
     });
     window.parent.postMessage(message, "*");
   } else {
