2b21e423e6fbff67697e5a7849f22a9cb171b754
satokiy <syamada@netprotections.co.jp>
2023-01-28 18:10:19 +0900

    [wip]ts化

---
+++ index.ts
@@ -0,0 +1,29 @@
+/**
+ * サイト側の実装を用意する
+ * 必要なのはボタンとコールバック
+ */
+import { openButtonEl } from "./src/components/openButton.js";
+import { start, config } from "./src/exApp.js";
+
+const openButton = openButtonEl();
+const app = document.getElementById('app') as HTMLDivElement;
+app.appendChild(openButton);
+
+// open modal
+openButton.addEventListener("click", function () {
+  start();
+});
+
+config({
+  cancel: () => {
+    console.log("cancel!!!");
+    window.location.href = "thanks.html";
+  },
+  goOther: (url) => {
+    console.log("go other!!!");
+    if (url) {
+      console.log(url);
+      window.location.href = url;
+    }
+  },
+});
